af55e26acf0b337d09a14179f097ea6cf910e3f3
Joyesh Debnath <itsmeJoyeshdebnath26@gmail.com>
2023-12-08 11:35:23 +0530

    recdux store and toolkit  setup

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+//store and redux
+import store from "./redux/store/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-	<React.StrictMode>
+	<Provider store={store}>
 		<App />
-	</React.StrictMode>
+	</Provider>
 );
