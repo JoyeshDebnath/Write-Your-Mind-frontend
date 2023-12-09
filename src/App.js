04118c6e8023bef703f42a357ccfd82427fd227e
Joyesh Debnath <itsmeJoyeshdebnath26@gmail.com>
2023-12-09 20:37:25 +0530

    added navigation for private , public and admin routes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
 import Register from "./components/Users/Register/Register";
 import Login from "./components/Users/Login/Login";
+import NavBar from "./components/Navigation/NavBar";
 
 function App() {
 	return (
 		<BrowserRouter>
+			<NavBar></NavBar>
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route exact path="/register" component={Register} />
---
+++ src/components/Navigation/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PublicNavigation from "./PublicNavigation/PublicNavigation";
+import PrivateNavigation from "./PrivateNavigation/PrivateNavigation";
+import AdminNavigation from "./AdminNavigation/AdminNavigation";
+
+const NavBar = () => {
+	return (
+		<>
+			<AdminNavigation />
+		</>
+	);
+};
+
+export default NavBar;
