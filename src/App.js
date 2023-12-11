4253e858e43bdae7aa1e4dcfcdf4c137caf56115
Joyesh Debnath <itsmeJoyeshdebnath26@gmail.com>
2023-12-11 12:51:21 +0530

    add category form component layout

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import HomePage from "./components/HomePage/HomePage";
 import Register from "./components/Users/Register/Register";
 import Login from "./components/Users/Login/Login";
 import NavBar from "./components/Navigation/NavBar";
+import AddNewCategory from "./components/Categories/AddNewCategory";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<NavBar></NavBar>
 			<Switch>
+				<Route exact path="/add-category" component={AddNewCategory} />
 				<Route exact path="/" component={HomePage} />
 				<Route exact path="/register" component={Register} />
 				<Route exact path="/login" component={Login} />
