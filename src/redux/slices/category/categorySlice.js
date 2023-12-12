60e800763397d84920adab797a6e3deab8add87f
Joyesh Debnath <itsmeJoyeshdebnath26@gmail.com>
2023-12-12 09:29:49 +0530

    Update and Delete category actions and reducers

--- src/redux/slices/category/categorySlice.js
+++ src/redux/slices/category/categorySlice.js
@@ -60,12 +60,69 @@ export const fetchAllCategoriesAction = createAsyncThunk(
 	}
 );
 
+//Update a category Action
+export const updateCategoryAction = createAsyncThunk(
+	"category/update-category",
+	async (categoryId, { rejectWithValue, getState, dispatch }) => {
+		const user = getState().users;
+		const { userAuth } = user;
+		const config = {
+			headers: {
+				Authorization: `Bearer ${userAuth?.token}`,
+			},
+		};
+
+		try {
+			const { data } = await axios.put(
+				`${baseUrl}/api/category/${categoryId}`,
+				config
+			);
+
+			return data;
+		} catch (error) {
+			if (!error?.response) {
+				throw error;
+			}
+			return rejectWithValue(error?.response?.data);
+		}
+	}
+);
+//Delete a category action .
+export const deleteCategoryAction = createAsyncThunk(
+	"category/delete-category",
+	async (categoryId, { rejectWithValue, getState, dispatch }) => {
+		const user = getState().users;
+		const { userAuth } = user;
+		const config = {
+			headers: {
+				Authorization: `Bearer ${userAuth?.token}`,
+			},
+		};
+
+		try {
+			const { data } = await axios.delete(
+				`${baseUrl}/api/category/${categoryId}`,
+				config
+			);
+
+			return data;
+		} catch (error) {
+			if (!error?.response) {
+				throw error;
+			}
+			return rejectWithValue(error?.response?.data);
+		}
+	}
+);
 //category slices
 
 const categorySlices = createSlice({
 	name: "categories",
 	initialState: { category: "all" },
 	extraReducers: (builder) => {
+		//-------------------------------------------------------------------------
+		//Create a Category
+		//------------------------------------------------------------------------
 		builder.addCase(createCategoryAction.pending, (state, action) => {
 			state.loading = true;
 			state.appErr = undefined;
@@ -102,6 +159,50 @@ const categorySlices = createSlice({
 		});
 
 		builder.addCase(fetchAllCategoriesAction.rejected, (state, action) => {
+			state.loading = false;
+			state.appErr = action?.payload?.message;
+			state.serverErr = action?.error?.message;
+		});
+
+		//-----------------------------------------------------------------
+		//Update Category Reducers
+		//---------------------------------------------------------------
+		builder.addCase(updateCategoryAction.pending, (state, action) => {
+			state.loading = true;
+			state.appErr = undefined;
+			state.serverErr = undefined;
+		});
+
+		builder.addCase(updateCategoryAction.fulfilled, (state, action) => {
+			state.loading = false;
+			state.appErr = undefined;
+			state.serverErr = undefined;
+			state.updatedCategory = action?.payload;
+		});
+
+		builder.addCase(fetchAllCategoriesAction.rejected, (state, action) => {
+			state.loading = false;
+			state.appErr = action?.payload?.message;
+			state.serverErr = action?.error?.message;
+		});
+
+		//-----------------------------------------------------------------
+		//Delete Category Reducers
+		//---------------------------------------------------------------
+		builder.addCase(deleteCategoryAction.pending, (state, action) => {
+			state.loading = true;
+			state.appErr = undefined;
+			state.serverErr = undefined;
+		});
+
+		builder.addCase(deleteCategoryAction.fulfilled, (state, action) => {
+			state.loading = false;
+			state.appErr = undefined;
+			state.serverErr = undefined;
+			state.deletedCategory = action?.payload;
+		});
+
+		builder.addCase(deleteCategoryAction.rejected, (state, action) => {
 			state.loading = false;
 			state.appErr = action?.payload?.message;
 			state.serverErr = action?.error?.message;
